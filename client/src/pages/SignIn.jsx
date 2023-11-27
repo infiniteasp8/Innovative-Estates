176ec1a7ae4e4ff5fc5ffbb23ce09da935d02fed
Anant Sharan Pandey <infiniteasp8>
2023-11-27 14:29:00 +0530

    add redux persist

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -28,8 +28,9 @@ export default function SignIn() {
       });     
       const data = await res.json();
       console.log(data);
-      if (data.success == false) {
+      if (data.success === false) {
         dispatch(signInFailure(data.message));
+        loading 
         return;
       }
       dispatch(signInSuccess(data));
@@ -59,7 +60,7 @@ export default function SignIn() {
         />
         <button
           disabled={loading}
-          className="bg-slate-700 rounded-lg text-white p-3 uppercase hover:opacity-90 disabled:opacity-80"
+          className="bg-slate-800 rounded-lg text-white p-3 uppercase hover:opacity-90 disabled:opacity-80"
         >
           {loading ? "Loading..." : "Sign In"}
         </button>
@@ -67,7 +68,7 @@ export default function SignIn() {
       <div className="flex gap-2 mt-5">
         <p>Do not have an account?</p>
         <Link to={"/sign-up"}>
-          <span className="text-blue-500">Sign Up</span>
+          <span className="text-blue-700">Sign Up</span>
         </Link>
       </div>
       {error && <p className="text-red-500 mt-5">{error}</p>}
