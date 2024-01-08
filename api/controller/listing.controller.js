38e18976b77270b99e7e3b20064595fab75171df
Anant Sharan Pandey <infiniteasp8>
2024-01-09 02:03:57 +0530

    add image slider to listing page

--- api/controller/listing.controller.js
+++ api/controller/listing.controller.js
@@ -50,7 +50,7 @@ export const updateListing = async (req, res, next) => {
   }
 };
 
-export const getListings = async (req, res, next) => {
+export const getListing = async (req, res, next) => {
 try {
   const listing = await Listing.findById(req.params.id);
   if (!listing) {
