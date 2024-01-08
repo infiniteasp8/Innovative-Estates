3ef38ce83416d855c9a97e3fff0670a4bf3c0224
Anant Sharan Pandey <infiniteasp8>
2024-01-08 23:03:11 +0530

    complete update listing functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import SignUp from './pages/SignUp';
 import Header from './components/Header';
 import PrivateRoute from './components/privateroute';
 import CreateListing from './pages/createListing';
+import UpdateListing from './pages/UpdateListing';
 
  
 export default function App () {
@@ -21,6 +22,10 @@ export default function App () {
     <Route element={<PrivateRoute/>}>
     <Route path= "/profile" element={<Profile/>}/>
     <Route path= "/create-listing" element={<CreateListing/>}/>
+    <Route
+            path='/update-listing/:listingId'
+            element={<UpdateListing />}
+          />
     </Route>
 
   </Routes>
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -249,10 +249,11 @@ export default function Profile() {
         {showListingsError ? 'Error showing listings' : ''}
       </p>
 
-      {userListings &&
-        userListings.length > 0 &&
-        <div className="flex flex-col gap-4">
-          <h1 className='text-center mt-7 text-2xl font-semibold'>Your Listings</h1>
+      {userListings && userListings.length > 0 && (
+        <div className='flex flex-col gap-4'>
+          <h1 className='text-center mt-7 text-2xl font-semibold'>
+            Your Listings
+          </h1>
           {userListings.map((listing) => (
             <div
               key={listing._id}
@@ -271,14 +272,22 @@ export default function Profile() {
               >
                 <p>{listing.name}</p>
               </Link>
-  
+
               <div className='flex flex-col item-center'>
-                <button onClick={()=>handleListingDelete(listing._id)} className='text-red-700 uppercase'>Delete</button>
-                <button className='text-green-700 uppercase'>Edit</button>
+                <button
+                  onClick={() => handleListingDelete(listing._id)}
+                  className='text-red-700 uppercase'
+                >
+                  Delete
+                </button>
+                <Link to={`/update-listing/${listing._id}`}>
+                  <button className='text-green-700 uppercase'>Edit</button>
+                </Link>
               </div>
             </div>
           ))}
-        </div>}
+        </div>
+      )}
     </div>
   );
 }
